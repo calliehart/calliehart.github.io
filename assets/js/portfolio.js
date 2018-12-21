90d03f992406f81ae1021fe58bbae61e968007d3
Callie Hart <callie_miller@hotmail.com>
2018-12-20 18:12:03 -0600

    trying to decide direction

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -15,20 +15,15 @@ function descriptiveLoop() {
 $("#changingTitles").append(descriptive[0]);
 setInterval(descriptiveLoop, 2000);
 
-$(document).ready(function(){
-    var scrollTop = 0;
-    $(window).scroll(function(){
-      scrollTop = $(window).scrollTop();
-       $('.counter').html(scrollTop);
-      
-      if (scrollTop >= 100) {
-        $('#global-nav').addClass('scrolled-nav');
-      } else if (scrollTop < 100) {
-        $('#global-nav').removeClass('scrolled-nav');
-      } 
-      
-    }); 
-    
-  });
+$("#projWrapper").show();
 
-    $("#processPage").hide();
+$("#UXBtn").on("click", function(){
+  $("#projWrapper").hide()
+
+});
+
+
+$("#WebBtn").on("click", function(){
+  $("#projWrapper").show()
+
+});
